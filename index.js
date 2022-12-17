7dc7a5448c82eb89bab1f483c9e9d011349f990e
Buvaneswaran S <99597201+Indherjith@users.noreply.github.com>
2022-12-17 12:00:18 +0530

    min

--- index.js
+++ index.js
@@ -4,51 +4,29 @@ const cors = require('cors');
 const { connection } = require("./config/db");
 const { ItemModel } = require("./Models/shop.model");
 const { BookmarkModel } = require("./Models/bookmark.model");
+const { JobModel } = require("./Models/Job.model");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/",async(req,res)=>{
-    let Items = await ItemModel.find();
-    res.send(Items)
+app.get("/",(req,res)=>{
+    res.send("welcome")
 })
 
-app.post("/addItem",async(req,res)=>{
+app.get("/jobs",async(req,res)=>{
+    let Jobs = await JobModel.find();
+    res.send(Jobs)
+})
+
+app.post("/addJobs",async(req,res)=>{
     var today = new Date();
-    const {Title,Quantity,Priority,Description} = req.body;
-    const date= today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    const DateTimestamp = date+' ['+time+']';
-    const item = new ItemModel({Title,Quantity,Priority,DateTimestamp,Description})
-    await item.save()
-    res.send("Item Added Successfully");
-})
-
-app.delete("/delete",async(req,res)=>{
-    let Items = await ItemModel.deleteOne(req.query)
-    res.send("Item Deleted Successfully")
-})
-
-app.get("/bookmark",async(req,res)=>{
-    let Items = await BookmarkModel.find();
-    res.send(Items)
-})
-
-app.post("/addBookmark",async(req,res)=>{
-    let Item = await ItemModel.find({_id:req.query._id});
-    const {Title,Quantity,Priority,DateTimestamp,Description} = Item[0];
-    let bookmark = await BookmarkModel.find({Title,Quantity,Priority,DateTimestamp,Description});
-    console.log(Item,bookmark)
-    if(bookmark==[]){
-        res.send("Item Already in Bookmark")
-    }
-    else{
-        const item = new BookmarkModel({Title,Quantity,Priority,DateTimestamp,Description})
-        console.log(item)
-        await item.save()
-        res.send("Item Added to Bookmark")
-    }
+    const {company,city,location,role,level,contract,position,language} = req.body;
+    const postedAt=today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    const job = new JobModel({company,postedAt,city,location,role,level,contract,position,language})
+    await job.save()
+    console.log(job)
+    res.send("Job Posted Successfull")
 })
 
 
